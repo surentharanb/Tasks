bb2c161125cd6546690f0cb2f7979c72edc3247b
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-02-24 11:48:32 +0530

    Create ***.c

---
+++ ***.c
@@ -0,0 +1,20 @@
+#include<conio.h>
+void main()
+{
+    int i,j;
+    clrscr();
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==1||j==1||i==5||j==5||i==j||i==2j||j==i/2||i==j/2)
+            {
+                printf("*");
+                printf("\t");
+            }
+            else
+            printf("\t");
+        }
+        printf("\n");
+    }
+}
