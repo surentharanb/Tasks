5b1a946e6eef0ac1e4925a1727d395f45c4657c7
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-02-24 11:59:41 +0530

    Create 0.c

---
+++ 0.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    float c;
+    printf("enter the decimal values of  the  item:");
+    scanf("%f",&c);
+    b=100;
+    
+    a=c*b;
+    printf("the paisa value of the item is %d",a);
+    getch();
+}
