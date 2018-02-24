082d325961ac2fc5d24ad6a3f87ecb2ae77d569c
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-02-24 11:56:50 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    int a[i];
+    int *b;
+    b=a[i];
+    printf("enter the values:");
+    for(i=0;i<3;i++){
+        
+   scanf("\n %d",&a[i]);
+        
+    }
+for(i=0;i<3;i++)
+{
+        printf("\n %d",b);
+        ++b;
+    }
+    
+}
